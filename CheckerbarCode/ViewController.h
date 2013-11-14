3175461ec8431727e680a9db484522ea4617876b
Aravind <armia@Armias-Mac-mini.local>
2013-11-14 17:46:38 +0530

    Initial Commit

---
+++ CheckerbarCode/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CheckerbarCode
+//
+//  Created by Aravind on 11/14/13.
+//  Copyright (c) 2013 Gizmoplay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CheckerbarCode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CheckerbarCode
+//
+//  Created by Aravind on 11/14/13.
+//  Copyright (c) 2013 Gizmoplay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

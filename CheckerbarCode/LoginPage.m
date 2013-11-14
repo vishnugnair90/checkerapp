b1d618fdc15fae9691253ba13161fe87ba110160
Aravind Nair <aravindgs007@gmail.com>
2013-11-14 18:09:32 +0530

    Changed name of initial view controller to LoginPage

--- CheckerbarCode/LoginPage.m
+++ CheckerbarCode/LoginPage.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2013 Gizmoplay. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LoginPage.h"
 
-@interface ViewController ()
+@interface LoginPage ()
 
 @end
 
-@implementation ViewController
+@implementation LoginPage
 
 - (void)viewDidLoad
 {
